30f9f3de05c2168fb1c9fa23d0026b48d8b6704c
Keith Kirtfield <keithkirtfield@Keiths-MacBook-Pro.local>
2020-03-26 19:36:46 -0400

    Added plan to main file

--- worklogger/src/main/java/com/worklogger_differences/worklogger/WorkloggerApplication.java
+++ worklogger/src/main/java/com/worklogger_differences/worklogger/WorkloggerApplication.java
@@ -2,7 +2,18 @@ package com.worklogger_differences.worklogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+	This Spring Boot Server handles the logic of difference
+	It holds three postgres tables
 
+	Files, content, and differences
+	The file contents must have the same file ID to be compared
+
+	Through the client -> files and their contents will populate this server's db
+	And this server will populate the difference between files
+	between git commits
+
+ */
 @SpringBootApplication
 public class WorkloggerApplication {
 
